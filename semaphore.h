4b90fc82c0799546b9ac30ac0f90138c2c176263
archqua <zakharovpnsd@mail.ru>
2022-07-08 13:29:38 +0300

    fixed weird use-after-free in semaphore.h

--- semaphore.h
+++ semaphore.h
@@ -5,24 +5,15 @@
 #include <mutex>
 
 class Semaphore {
-  // bruh 2 of 3 members are mutable...
   mutable std::mutex mx;
   int _count = 0;
-  mutable bool locked = false;
 public:
-  class Lock: std::unique_lock<std::mutex> {
-    using Base = std::unique_lock<std::mutex>;
-    const Semaphore& host;
-  public:
-    Lock(const Semaphore& h): Base(h.mx), host(h) { host.locked=true; }
-    Lock(Lock&& other) = default;
-    ~Lock() { host.locked=false; }
-  };
+  using Lock = std::unique_lock<std::mutex>;
   Semaphore(int c = 0): _count(c) {}
-  Lock lock() const { return Lock(*this); }
+  Lock lock() const { return Lock(mx); }
   int countUnsafe() const { return _count; }
   int count() const { auto l = lock(); return countUnsafe(); }
-  bool isLocked() const { return locked; }
+  // bool isLocked() const { return locked; }
   int incUnsafe() { return ++_count; }
   int decUnsafe() { return --_count; }
   int inc() { auto l = lock(); return incUnsafe(); }
